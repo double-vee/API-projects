e04910466942c13bd0ee82377c42062f6a963a29
Weronika Kansy <weronika.kansy@gmail.com>
2022-01-08 18:04:50 +0100

    Instantiated an object, logged user data to the console with the object method getUser()

--- github-user-finder/app.js
+++ github-user-finder/app.js
@@ -1,8 +1,18 @@
+const github = new Github();
+
 const searchUser = document.getElementById("searchUser");
 
 searchUser.addEventListener("keyup", (e) => {
   const input = e.target.value.trim();
   if (input !== "") {
-    console.log(input);
+    github
+      .getUser(input)
+      .then((data) => {
+        if (data.profile.message === "Not Found") {
+        } else {
+          console.log(data.profile);
+        }
+      })
+      .catch(console.log);
   }
 });
