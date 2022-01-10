61be8c4d3480c11b8d5f08a88c367c47939a3171
Weronika Kansy <weronika.kansy@gmail.com>
2022-01-11 00:03:32 +0100

    Repos fetched inside the getUser() method

--- github-user-finder/github.js
+++ github-user-finder/github.js
@@ -1,9 +1,19 @@
 class Github {
+  constructor() {
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`
+    );
     const profileData = await profileResponse.json();
+    const repoData = await repoResponse.json();
     return {
       profile: profileData,
+      repos: repoData,
     };
   }
 }
