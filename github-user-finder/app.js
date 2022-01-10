92ab5a79fdf6b64de16e4ba7584729b81f4c9415
Weronika Kansy <weronika.kansy@gmail.com>
2022-01-10 16:54:36 +0100

    Alert displayed with the showAlert() method when the user has not been found

--- github-user-finder/app.js
+++ github-user-finder/app.js
@@ -6,16 +6,12 @@ const searchUser = document.getElementById("searchUser");
 searchUser.addEventListener("keyup", (e) => {
   const input = e.target.value.trim();
   if (input !== "") {
-    github
-      .getUser(input)
-      .then((data) => {
-        if (data.profile.message === "Not Found") {
-        } else {
-          ui.showProfile(data.profile);
-        }
-      })
-      .catch(console.log);
-  } else {
-    ui.clearProfile();
+    github.getUser(input).then((data) => {
+      if (data.profile.message === "Not Found") {
+        ui.showAlert("User not found", "alert alert-danger");
+      } else {
+        ui.showProfile(data.profile);
+      }
+    });
   }
 });
--- github-user-finder/github.js
+++ github-user-finder/github.js
@@ -1,11 +1,6 @@
 class Github {
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}`);
-    if (!profileResponse.ok) {
-      throw Error(
-        `Something went wrong: ${profileResponse.status} ${profileResponse.statusText}`
-      );
-    }
     const profileData = await profileResponse.json();
     return {
       profile: profileData,
