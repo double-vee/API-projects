c2edcbdb732f0ed80491d9baa213feccead47957
Weronika Kansy <weronika.kansy@gmail.com>
2022-01-24 21:58:12 +0100

    Min/max temperature, visibility and pressure included in the UI class; temp. calculated with Math.floor(), units hardcoded in the UI, icon link corrected

--- weather-app/ui.js
+++ weather-app/ui.js
@@ -2,25 +2,33 @@ class UI {
   constructor() {
     this.location = document.getElementById("w-location");
     this.desc = document.getElementById("w-desc");
-    this.string = document.getElementById("w-string");
+    this.temp = document.getElementById("w-temp");
+    this.tempMin = document.getElementById("w-temp-min");
+    this.tempMax = document.getElementById("w-temp-max");
     this.icon = document.getElementById("w-icon");
     this.feelsLike = document.getElementById("w-feels-like");
     this.wind = document.getElementById("w-wind");
     this.humidity = document.getElementById("w-humidity");
+    this.pressure = document.getElementById("w-pressure");
+    this.visibility = document.getElementById("w-visibility");
   }
 
   paint(weatherData) {
-    const { main, weather, wind, sys, name } = weatherData;
+    const { main, weather, wind, visibility, sys, name } = weatherData;
 
     this.location.textContent = `${name}, ${sys.country}`;
     this.desc.textContent = weather[0].description;
-    this.string.textContent = main.temp;
+    this.temp.textContent = `${Math.floor(main.temp)}°C`;
+    this.tempMin.textContent = `Min: ${Math.floor(main.temp_min)}°C`;
+    this.tempMax.textContent = `Max: ${Math.floor(main.temp_max)}°C`;
     this.icon.setAttribute(
       "src",
-      `http://openweathermap.org/img/w/${weather[0].icon}.png`
+      `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
     );
-    this.feelsLike.textContent = `Feels like: ${main.feels_like}`;
-    this.wind.textContent = `Wind: ${wind.speed}`;
-    this.humidity.textContent = `Humidity: ${main.humidity}`;
+    this.feelsLike.textContent = `Feels like: ${Math.floor(main.feels_like)}°C`;
+    this.wind.textContent = `Wind: ${wind.speed} m/s`;
+    this.humidity.textContent = `Humidity: ${main.humidity}%`;
+    this.pressure.textContent = `Pressure: ${main.pressure} hPa`;
+    this.visibility.textContent = `Visibility: ${visibility} m`;
   }
 }
