61b93178191c21218fcde6907ee9b6c096b93529
Weronika Kansy <weronika.kansy@gmail.com>
2022-01-25 22:05:01 +0100

    Added a click event listener to the save button inside the modal to trigger the update of city and state and the display of new data

--- weather-app/app.js
+++ weather-app/app.js
@@ -9,3 +9,16 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .catch(console.log);
 });
+
+document.getElementById("w-change-btn").addEventListener("click", (e) => {
+  const city = document.getElementById("city").value;
+  const state = document.getElementById("state").value;
+
+  weather.changeLocation(city, state);
+  weather
+    .getWeather()
+    .then((result) => {
+      ui.paint(result);
+    })
+    .catch(console.log);
+});
