16d8117fefe4bcc935be730a8b0ef037145ea1d1
Weronika Kansy <weronika.kansy@gmail.com>
2022-01-25 22:13:22 +0100

    Refactored app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,24 +1,21 @@
 const weather = new Weather("Warsaw", "PL", "metric");
 const ui = new UI();
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", showWeather);
+
+function showWeather() {
   weather
     .getWeather()
     .then((result) => {
       ui.paint(result);
     })
     .catch(console.log);
-});
+}
 
 document.getElementById("w-change-btn").addEventListener("click", (e) => {
   const city = document.getElementById("city").value;
   const state = document.getElementById("state").value;
 
   weather.changeLocation(city, state);
-  weather
-    .getWeather()
-    .then((result) => {
-      ui.paint(result);
-    })
-    .catch(console.log);
+  showWeather();
 });
