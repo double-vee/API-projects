0bd708a0b7bec0d8b93d1a7cd0748e508d07e5c2
Weronika Kansy <weronika.kansy@gmail.com>
2022-04-12 20:43:12 +0200

    Fixed dates in profile info using a regex and the exec() function; added bio; displayed an empty string instead of null when there is no data

--- github-user-finder/ui.js
+++ github-user-finder/ui.js
@@ -10,21 +10,38 @@ class UI {
         <div class="row">
           <div class="img-btn-wrap col-md-3">
             <img class="img img-fluid mb-3" src="${user.avatar_url}">
-            <a href="${user.html_url}" target="_blank" class="btn btn-primary w-100">View profile</a>
+            <a href="${
+              user.html_url
+            }" target="_blank" class="btn btn-primary w-100">View profile</a>
           </div>
           <div class="col-md-9">
             <div class="px-3 d-flex flex-wrap gap-2 mb-3">
-              <span class="badge rounded-pill">Public repos: ${user.public_repos}</span>
-              <span class="badge rounded-pill">Public gists: ${user.public_gists}</span>
-              <span class="badge rounded-pill">Followers: ${user.followers}</span>
-              <span class="badge rounded-pill">Following: ${user.following}</span>
+              <span class="badge rounded-pill">Public repos: ${
+                user.public_repos
+              }</span>
+              <span class="badge rounded-pill">Public gists: ${
+                user.public_gists
+              }</span>
+              <span class="badge rounded-pill">Followers: ${
+                user.followers
+              }</span>
+              <span class="badge rounded-pill">Following: ${
+                user.following
+              }</span>
             </div>
             <ul class="list-group">
-              <li class="list-group-item">Company: ${user.company}</li>
+              <li class="list-group-item">Company: ${
+                !user.company ? "" : user.company
+              }</li>
               <li class="list-group-item">Blog: ${user.blog}</li>
+              <li class="list-group-item">Bio: ${!user.bio ? "" : user.bio}</li>
               <li class="list-group-item">Location: ${user.location}</li>
-              <li class="list-group-item">Member since: ${user.created_at}</li>
-              <li class="list-group-item">Last update: ${user.updated_at}</li>
+              <li class="list-group-item">Member since: ${
+                /\d{4}-\d{2}-\d{2}/.exec(user.created_at)[0]
+              }</li>
+              <li class="list-group-item">Last update: ${
+                /\d{4}-\d{2}-\d{2}/.exec(user.updated_at)[0]
+              }</li>
             </ul>
           </div>
         </div>
